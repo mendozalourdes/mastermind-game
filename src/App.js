bfc2db3ace494e23ebe16922e9ee75e0fef696e4
mendozalourdes <lourd.mendoza@gmail.com>
2022-04-27 22:26:15 -0600

    Edit app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ import * as React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
-      </header>
+        </h1>
     </div>
   );
 }
