1eee12c041dafcd89c06b4cf88a7605ca9073b37
mendozalourdes <lourd.mendoza@gmail.com>
2022-05-01 20:16:05 -0600

    Include new row to be shown on board game

--- src/Components/BoardGame.js
+++ src/Components/BoardGame.js
@@ -1,22 +1,41 @@
 import React from "react";
 import SingleBoardPiece from "./SingleBoardPiece";
 import { useState } from "react";
+import { colors } from "../Utils/colors";
+import Row from "./Row";
 
-const BoardGame = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const totalRows = [];
-  for (let i = 0; i < 10; i++) {
-    totalRows.push(
-      <SingleBoardPiece
-        key={"row_" + i}
-        id={"row_" + i}
-        // userGuess = {userGuess}
-      />
-    );
-  }
+const BoardGame = ({allUserGuesses, secretCode}) => {
 
-  return <div className="board-game">{totalRows}</div>;
+  let everyGuessRow = allUserGuesses.map((guess, i) => {
+    
+        return  <Row
+                    key={"row_" + i}
+                    id={i}
+                    guess={guess}
+                    allUserGuesses={allUserGuesses[i]}
+                  />
+
+  
+})
+
+
+//   const totalRows = [];
+//   for (let i = 0; i < 10; i++) {
+//     totalRows.push(
+//       <Row
+//         key={"row_" + i}
+//         id={"row_" + i}
+//         // userGuess = {userGuess}
+//       />
+//     );
+//   }
+
+  return <div className="board-game">
+       {/* <div className="one-guess-section">  */}
+       {everyGuessRow}
+      
+      </div>;
 };
 
 export default BoardGame;
