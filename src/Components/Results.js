2297354e132651a7e8067042c08025a439573ea0
mendozalourdes <lourd.mendoza@gmail.com>
2022-05-04 16:54:56 -0600

    Add results component.

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useState } from "react";
 
-const Results = () => {
+const Results = ({allUserGuesses}) => {
+    const [gameOver, setGameover] = useState(false);
+
+    const countEveryGuess = () => {
+
+        if(allUserGuesses.length === 10 ) {
+            setGameover(true)
+        }
+    }
+
+// console.log("???",  countEveryGuess(), gameOver)
     return (
-        <div>
-            
-        </div>
+        <section className="results">
+            <h2>Results</h2>
+            <div className="remaining-guesses">
+                <p>Remaining Guesses</p>
+            <p>{10 - allUserGuesses.length}</p>
+
+            </div>
+        {gameOver && <h3>Game Over!</h3>}
+        </section>
     );
 };
 
